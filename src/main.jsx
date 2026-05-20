12667131fdf5d1f8add1d7733418b91216561e47
Quoc Bao Dinh Le <q.le102100@gmail.com>
2026-05-20 11:33:28 -0400

    chore: update browser tab favicon

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<link rel="icon" type="image/svg+xml" href="/koi-fish.svg" />
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
