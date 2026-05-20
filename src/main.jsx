64d586e0f60616564f5f1b0a838afcc936a4f740
Quoc Bao Dinh Le <q.le102100@gmail.com>
2026-05-20 11:26:12 -0400

    chore: update browser tab favicon
    - Added new logo/icon image to the public directory.
    - Updated the <link rel="icon"> tag in index.html to reference the new favicon file instead of the default koi-fish.svg.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+<link rel="icon" type="image/svg+xml" href="/koi-fish.svg" />
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
